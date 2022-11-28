5dfdb4821a4789b6c85eb216b4486293c5f2883b
rasmusbothomsen <rasmus0408@gmail.com>
2022-11-28 17:54:16 +0100

    initial commit

---
+++ app/src/main/java/com/example/wheeloffortune/view/GamePage.kt
@@ -0,0 +1,17 @@
+package com.example.wheeloffortune.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class GamePage {
+
+
+
+
+
+    @Preview
+    @Composable
+    fun GamePagePreview(){
+
+    }
+}
---
+++ app/src/main/java/com/example/wheeloffortune/viewmodel/GamePageViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.wheeloffortune.viewmodel
+
+class GamePageViewModel {
+
+}
